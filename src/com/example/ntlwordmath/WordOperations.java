056631af8380cae20e34427e78ece3dcf1eb07b5
npal312 <102007313+npal312@users.noreply.github.com>
2024-10-08 21:14:01 -0400

    Cut down scope
    Multiple types of maps aren't needed right now, I need to focus on one at a time (base 26 seems like a good one and fun to do)

--- src/com/example/ntlwordmath/WordOperations.java
+++ src/com/example/ntlwordmath/WordOperations.java
@@ -1,22 +1,10 @@
 package com.example.ntlwordmath;
 
-import com.example.num2let.WordMap;
-
 
 public class WordOperations {
-	private WordMap wMap;
 	
-	public WordOperations(WordMap wMap) {
-        this.setwMap(new WordMap());
+	public WordOperations() {
     }
-	
-	public WordMap getwMap() {
-		return wMap;
-	}
-
-	public void setwMap(WordMap wMap) {
-		this.wMap = wMap;
-	}
 	
 	public String add(String word1, String word2) {
 		//– Adds the numeric values of the two words and returns the result as a word.
@@ -33,9 +21,6 @@ public class WordOperations {
 	public String divide(String word1, String word2) {
 		//– Divides the numeric value of the first word by the second and returns the result.
 		return "";
-	}
-	public void setWordMap(WordMap wMap) {
-		//– Allows users to choose the letter mapping system.
 	}
 
 
---
+++ src/com/example/num2let/Convert.java
@@ -0,0 +1,14 @@
+package com.example.num2let;
+
+public class Convert {
+	//Base-26 conversion type
+	//May need BigInteger for this
+	public String ntlConvert(int number) {
+		//– Converts a number to its corresponding letter representation.
+		return "";
+	}
+	public int ltnConvert(String letters) {
+		//– Converts a string of letters back to its corresponding numeric value based on the chosen representation system.
+		return 0;
+	}
+}
